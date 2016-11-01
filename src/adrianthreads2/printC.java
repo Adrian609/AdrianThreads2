9595f7e074b390eeffe61a84345a938c16430f71
Adrian609 <rodriguez.adrian609@gmail.com>
2016-10-31 22:12:53 -0400

    Initial

---
+++ src/adrianthreads2/printC.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adrianthreads2;
+
+/**
+ *
+ * @author Arod6
+ */
+public class printC implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("C");
+    }
+
+}
